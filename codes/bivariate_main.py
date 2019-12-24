b515ca6ba758a3fbaace691ca60b763d53d9abb2
bhavya <bhavyabhatt17@gmail.com>
2019-12-24 23:39:31 +0530

    final changes

--- codes/bivariate_main.py
+++ codes/bivariate_main.py
@@ -5,6 +5,7 @@ import pandas
 import numpy as np
 from torch.utils.data import Dataset, DataLoader, TensorDataset, random_split
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 
 def mean_squared_error(y_pred, y_true):
@@ -17,8 +18,7 @@ def mean_squared_error(y_pred, y_true):
 
 
 def cross_entropy_loss(y_pred, y_true):
-    # TODO
-    return 0
+    return -1 * torch.sum(y_true * torch.log(y_pred))
 
 
 def data_preprocessing(X, y, batch_size, validation_split=0.2):
@@ -50,34 +50,40 @@ def fit(train_loader, val_loader, num_dim, num_epochs, num_steps, learning_rate)
 
     # defining parameters of the model
     mean_coeff = torch.randn(1, num_dim)
-    cov_coeff = torch.randn(num_dim, num_dim)
+    lower_coeff = torch.randn(int(num_dim * (num_dim + 1) / 2))  # vectorized lower triangular matrix
     a = torch.randn(3, 1)
     b = torch.randn(3, 1)
 
     # declaring them as learnable parameters
     mean_coeff.requires_grad = True
-    cov_coeff.requires_grad = True
+    lower_coeff.requires_grad = True
     a.requires_grad = True
     b.requires_grad = True
 
     # setting up the optimizer
-    parameters = [mean_coeff, cov_coeff, a, b]
+    parameters = [mean_coeff, lower_coeff, a, b]
     optimizer = optim.Adam(parameters, learning_rate)
 
     # mean and cov_mat batches calculations - mid point riemannian sum
-    c_b = (1 / num_steps) * torch.arange(1, 2 * (num_steps + 1), 2)
+    c_b = (1 / num_steps) * torch.arange(1, 2 * num_steps + 1, 2)
     c_b = c_b.view(-1, 1)
     c_matrix = torch.cat((c_b, c_b ** 2, c_b ** 3), 1)
     a_matrix = torch.mm(c_matrix, a)
     b_matrix = torch.mm(c_matrix, b)
+    
+    # forming lower triangular matrix
+    lower_indices = torch.tril_indices(num_dim, num_dim)
+    lower_matrix = torch.zeros((num_dim, num_dim))
+    lower_matrix[lower_indices[0], lower_indices[1]] = lower_coeff
 
     # defining batch multivariate normal distribution
     mean = torch.mm(
         a_matrix, mean_coeff
     )  # batch mean along all values of c_b step varying from 0 to 1
-    cov_mat = (b_matrix * cov_coeff.view(1, -1)).view(
+    cov_mat_l = (b_matrix * lower_matrix.view(1, -1)).view(
         -1, num_dim, num_dim
-    )  # batch covariance matrix
+    )  # batch cholesky lower triangular matrix
+    cov_mat = torch.matmul(cov_mat_l, torch.transpose(cov_mat_l, 1, 2))  # cholesky decomposition
     p = torch.distributions.multivariate_normal.MultivariateNormal(
         mean, cov_mat
     )  # batch normal distribution
@@ -86,10 +92,12 @@ def fit(train_loader, val_loader, num_dim, num_epochs, num_steps, learning_rate)
     train_losses, val_losses, epochs = [], [], []
     train_len = len(train_loader)
     val_len = len(val_loader)
+    print("Training starting !")
     for epoch_index in range(num_epochs):
         print("Epoch no. ", epoch_index)
         train_loss = 0
         val_loss = 0
+        batch_count = 0
         for x, y in train_loader:
             optimizer.zero_grad()
             batch_size = x.shape[0]
@@ -97,12 +105,13 @@ def fit(train_loader, val_loader, num_dim, num_epochs, num_steps, learning_rate)
                 x.repeat(1, num_steps).view(-1, num_dim).view(batch_size, -1, num_dim)
             )
             y_pred = (1 / num_steps) * torch.sum(torch.exp(p.log_prob(x_repeat)), dim=1)
-            loss = mean_squared_error(
+            loss = cross_entropy_loss(
                 y_pred, y
             )  # loss function between predicted and true values
-            loss.backward()  # calculate gradients
+            loss.backward(retain_graph=True)  # calculate gradients
             optimizer.step()  # take a small step in the direction of gradient
             train_loss += loss.item()
+            batch_count += 1
         else:
             with torch.no_grad():
                 # scope of no gradient calculations
@@ -116,23 +125,28 @@ def fit(train_loader, val_loader, num_dim, num_epochs, num_steps, learning_rate)
                     y_pred = (1 / num_steps) * torch.sum(
                         torch.exp(p.log_prob(x_repeat)), dim=1
                     )
-                    loss = mean_squared_error(y_pred, y)
+                    loss = cross_entropy_loss(y_pred, y)
                     val_loss += loss.item()
 
             train_losses.append(train_loss / train_len)
             val_losses.append(val_loss / val_len)
             epochs.append(epoch_index)
+            print("Train loss: %.2f - Val loss: %.2f" % (train_loss / train_len, val_loss / val_len))
 
-    print("training finished !")
+    print("Training finished !")
     
-    return mean_coeff, cov_coeff, a, b
-    
-    
+    plt.plot(epochs, train_losses, color='red')
+    plt.plot(epochs, val_losses, color='blue')
+    plt.show()
+
+    return mean_coeff, lower_coeff, a, b
+
+
 def main():
     # load the dataset
-    X = np.load("/data/x.npy")
-    y = np.load("/data/y.npy").reshape(-1, 1)
-    
+    X = np.load("data/x.npy")
+    y = np.load("data/y.npy").reshape(-1, 1)
+
     # defining hyperparameters
     batch_size = 100
     validation_split = 0.2
@@ -140,14 +154,15 @@ def main():
     num_epochs = 50
     num_steps = 500
     learning_rate = 0.1
-    
+
     # prepare dataloader for optimization
     train_loader, val_loader = data_preprocessing(X, y, batch_size, validation_split)
-    
+
     # training the model
-    mean_coeff, cov_coeff, a, b = fit(train_loader, val_loader, num_dim, num_epochs, num_steps, learning_rate) 
-    
-    
+    mean_coeff, cov_coeff, a, b = fit(
+        train_loader, val_loader, num_dim, num_epochs, num_steps, learning_rate
+    )
+
+
 if __name__ == "__main__":
     main()
-
--- codes/data/preprocessing.py
+++ codes/data/preprocessing.py
@@ -12,21 +12,21 @@ y = y.reshape(-1, 1).repeat(300, 0)
 
 xy = np.concatenate((x, y), 1)
 
-filepath = 'CorrelationData.txt'
+filepath = "CorrelationData.txt"
 values = []
 with open(filepath) as fp:
     line = fp.readline()
     count = 1
     while line:
         if count > 1:
-            z = line.split('\t')
+            z = line.split("\t")
             c = z[-1]
             digit = float(c[:-1])
             print(digit)
             values.append(int(digit))
         count += 1
         line = fp.readline()
-        
+
 final_values = []
 start = 50
 l = len(values)
@@ -46,26 +46,25 @@ c = np.load("x.npy")
 d = np.load("y.npy").reshape(-1, 1)
 
 from mpl_toolkits import mplot3d
-ax = plt.axes(projection='3d')
-ax.plot3D(x, y, final_values, 'gray')
 
-fig = plt.figure(figsize=(6,5))
+ax = plt.axes(projection="3d")
+ax.plot3D(x, y, final_values, "gray")
+
+fig = plt.figure(figsize=(6, 5))
 left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
-ax = fig.add_axes([left, bottom, width, height]) 
+ax = fig.add_axes([left, bottom, width, height])
 
 
 cp = ax.contour(x, y, final_values)
-ax.clabel(cp, inline=True, 
-          fontsize=10)
-ax.set_title('Contour Plot')
-ax.set_xlabel('x (cm)')
-ax.set_ylabel('y (cm)')
+ax.clabel(cp, inline=True, fontsize=10)
+ax.set_title("Contour Plot")
+ax.set_xlabel("x (cm)")
+ax.set_ylabel("y (cm)")
 plt.show()
 
-plt.contour(x, y, values, colors='black', linestyles='dashed')
-plt.clabel(cp, inline=True, 
-          fontsize=10)
-plt.title('Contour Plot')
-plt.xlabel('x (cm)')
-plt.ylabel('y (cm)')
+plt.contour(x, y, values, colors="black", linestyles="dashed")
+plt.clabel(cp, inline=True, fontsize=10)
+plt.title("Contour Plot")
+plt.xlabel("x (cm)")
+plt.ylabel("y (cm)")
 plt.show()
